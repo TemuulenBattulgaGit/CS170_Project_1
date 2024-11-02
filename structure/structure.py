c846145382ce7fc02dbb83d5cbdebe18d05be48e
hernantech <58359753+hernantech@users.noreply.github.com>
2024-11-01 23:40:50 -0700

    sorry tim I tried three different things but I think I'll have to abandon original plan and just put all the stuff in the solving function

--- structure/structure.py
+++ structure/structure.py
@@ -1,5 +1,7 @@
 import numpy
 import sys
+from collections import deque
+from statelist import StateList
 ###how do we make a 3x3 grid into a class?
 
 class Structure:
@@ -20,7 +22,7 @@ class Structure:
 
         '''For tracking state, etc'''
         self.state_hash = None
-
+        self.tracking = StateList()
     def askforgrid(self):
         print("Please enter your puzzle, using a zero to represent the blank")
         print("Enter the first row, use spaces or tabs between numbers")
@@ -62,6 +64,26 @@ class Structure:
         self.grid[self.where_is_zero], self.grid[self.where_is_zero - 3] = self.grid[self.where_is_zero - 3], self.grid[self.where_is_zero]
     def swap_down(self):
         self.grid[self.where_is_zero], self.grid[self.where_is_zero + 3] = self.grid[self.where_is_zero + 3], self.grid[self.where_is_zero]
+    '''Need list of valid moves'''
+    def get_valid_moves(self):
+        #since we can categorize the moves into corner/edge/center
+        #I'm gonna hardcode the possible moves rather than checking for legal/illegal moves each state
+        valid_moves = {
+            # Corners
+            0: ['right', 'down'],
+            2: ['left', 'down'],
+            6: ['right', 'up'],
+            8: ['left', 'up'],
+            # Edges
+            1: ['left', 'right', 'down'],
+            3: ['right', 'up', 'down'],
+            5: ['left', 'up', 'down'],
+            7: ['left', 'right', 'up'],
+            # Center
+            4: ['left', 'right', 'up', 'down']
+        }
+        return valid_moves[self.where_is_zero]
+
 
     #goal state is here
     def are_we_there_yet(self):
@@ -76,3 +98,41 @@ class Structure:
             val = val*9 + int(num)
         self.state_hash = val
 
+    #will try to use BFS to test the structure and see how it scales
+    def solving_using_bfs(self):
+        #using a priority queue
+        queue = deque([(self.grid, self.where_is_zero, [])])
+        self.tracking = StateList()
+
+        #adding the first state
+        initial_hash = self.state_to_hash(self.grid)
+        self.tracking.__insert__(initial_hash)
+
+        while queue:
+            #getting current state and where zero is
+            current_grid, current_zero_place, path = queue.popleft()
+            #updating state
+
+'''
+#putting this function on hold until I can debug the bfs function and get moves working
+    def process_solution(self, solution_path):
+        #Printing the solution path
+        if solution_path is None:
+            print("No solution found... we probably coded something wrong")
+            return
+
+        print(f"Solution found in {len(solution_path)} moves!")
+        print("Initial state:")
+        print(self.grid.reshape(3, 3))
+
+        current_grid_state = self.grid.copy()
+        current_zero_place = self.where_is_zero
+
+        for i, move in enumerate(solution_path, 1):
+            print(f"\nMove {i}: {move}")
+            #current_grid, current_zero_place = self.move(current_grid, current_zero_place, move)
+            #rewriting make_move function, sorry tim pls wait
+            print(current_grid.reshape(3, 3))
+
+        return solution_path
+'''
