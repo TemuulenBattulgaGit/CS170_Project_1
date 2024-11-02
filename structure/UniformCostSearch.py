7d67a4db9076d2ba04133fd039a1af63a3d61248
TemuulenBattulgaGit <temuulenbattulga@1209gmail.com>
2024-11-01 22:31:00 -0700

    added explored list

--- structure/UniformCostSearch.py
+++ structure/UniformCostSearch.py
@@ -6,3 +6,10 @@ class UniformCostSearch(structure):
         self.explored_states = []
         self.frontier = []
         self.goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+
+    def add_to_explored(self, state):
+        if state not in self.explored_states:
+            self.explored_states.append(state)
+
+    def is_explored(self, state):
+        return state in self.explored_states
