f3fe0791c7068d1c5967e65722f18fe1331d487f
TemuulenBattulgaGit <temuulenbattulga@1209gmail.com>
2024-11-01 21:29:46 -0700

    new stuff

--- costfunction/costfunction.py
+++ costfunction/costfunction.py
@@ -1,10 +1,17 @@
-import numpy
-import sys
+import numpy as np
 from structure import Structure
 
-class CostFunction(Structure):
-    def __init__(self, Structure):
-        # get the structure to access the grid
-        self.initial_stat = Structure
-        # defining the goal state
-        self.goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+class UniformCostSearch(Structure):
+    def __init__(self, start_state, goal_state=[1, 2, 3, 4, 5, 6, 7, 8, 0]):
+        super().__init__()
+        self.start_state = start_state
+        self.goal_state = tuple(goal_state)
+        self.frontier = []  #priority queue
+        self.explored = set()  #set of explored nodes
+        self.expanded_nodes = 0  #for keeping track of total number of nodes expanded 
+
+
+        heapq.heappush(self.frontier, (0, self.start_state, []))  #cost, state, path to reach
+        
+        while self.frontier:
+            cost, current_state, path = heapq.heappop(self.frontier)
