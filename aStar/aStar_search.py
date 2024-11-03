cf600804d29118e4a77721bfee00e80aaf9b13f5
TemuulenBattulgaGit <temuulenbattulga@1209gmail.com>
2024-11-02 22:37:50 -0700

    added missing_tile_heuristic

--- aStar/aStar_search.py
+++ aStar/aStar_search.py
@@ -9,3 +9,10 @@ class AStarSearch(structure):
         self.set_grid(start_state)
         self.goal_state = goal_state if goal_state else [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
+    def missing_tile_heuristic(self, state):
+        """counts the number of tiles out of place compared to the goal state."""
+        heuristic_cost = 0
+        for i in range(9):
+            if state[i] != 0 and state[i] != self.goal_state[i]:  #gotta ignore blank tile
+                heuristic_cost +=1
+        return heuristic_cost
